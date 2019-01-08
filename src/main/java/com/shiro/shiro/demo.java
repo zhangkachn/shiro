757447684b2bd7ddd95fb11cee62bcb0f7cbac03
zhangkang56 <zhangkachn@gmail.com>
2019-01-08 14:01:55 +0800

    登录设置

--- src/main/java/com/shiro/shiro/demo.java
+++ src/main/java/com/shiro/shiro/demo.java
@@ -4,6 +4,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.junit.Test;
@@ -94,7 +95,35 @@ public class demo {
         //6、退出
         subject.logout();
     }
+    
+  
+    @Test  
+    public void testAllSuccessfulStrategyWithSuccess() {  
+        login("classpath:shiro-authenticator-all-success.ini");  
+        Subject subject = SecurityUtils.getSubject();  
+      
+        //得到一个身份集合，其包含了Realm验证成功的身份信息  
+        PrincipalCollection principalCollection = subject.getPrincipals();  
+        Assert.assertEquals(2, principalCollection.asList().size());  
+    }   
+    
+    
+    private void login(String configFile) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
 
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+    }
+    
 
 	
 
